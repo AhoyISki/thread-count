907edbdf0416eb6bd6d2340287cce2e1a2520ebb
AhoyISki <ahoyiski@gmail.com>
2025-06-25 13:17:10 -0300

    Fix for macOS. Fixes AhoyISki/duat#7.

--- src/osx.rs
+++ src/osx.rs
@@ -1,5 +1,4 @@
 use std::num::NonZeroUsize;
-use std::ptr;
 
 use mach2::kern_return::KERN_SUCCESS;
 use mach2::task::task_threads;
@@ -9,7 +8,7 @@ pub(crate) fn thread_count() -> Option<NonZeroUsize> {
     let mut state = [0u32; 1296];
     let mut count: u32 = 0;
     let rc =
-        unsafe { task_threads(mach_task_self(), &mut state as *mut *mut u32, &mut count as *mut _) };
+        unsafe { task_threads(mach_task_self(), &mut state.as_mut_ptr() as *mut *mut u32, &mut count as *mut _) };
 
     if rc == KERN_SUCCESS {
         NonZeroUsize::new(usize::try_from(count).expect("Failed converting from u32 to usize"))
